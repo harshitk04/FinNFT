6640162b78f685949b71d9df267d70d913d75773
harshitk04 <harshitk04.hr@gmail.com>
2025-02-23 00:05:06 +0530

    Init

---
+++ ai_nft_generator/src/App.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import { NFTStorage, File } from 'nft.storage'
+import { Buffer } from 'buffer';
+import { ethers } from 'ethers';
+import axios from 'axios';
+
+// Components
+import Spinner from 'react-bootstrap/Spinner';
+import Navigation from './components/Navigation';
+
+// ABIs
+import NFT from './abis/NFT.json'
+
+// Config
+import config from './config.json';
+
+function App() {
+  const [provider, setProvider] = useState(null)
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
+  return (
+    <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <div className='form'>
+        <form>
+          <input type='text' placeholder='Create a name....'></input>
+          <input type='text' placeholder='Create a Description....'></input>
+          <input type='submit' value='Create & Mint'></input>
+        </form>
+        <div className='image'>
+          <img src="" alt="AI generated image"/>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
